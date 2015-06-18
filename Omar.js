d28af89f4f3d5c2d1f6ad7f40e67192c7898f906
dev-pauls <paul.sealock@xero.com>
2015-06-19 08:13:36 +1200

    editorconfig

--- Omar.js
+++ Omar.js
@@ -1,74 +1,74 @@
 var fs = require('fs');
 
 var Omar = (function () {
-	/*
-	Private functions
-	*/
+  /*
+  Private functions
+  */
 
 
 
 
-	/*
-	Public functions
-	*/
-	return {
-		/*
-		Helper function
-		*/
-		writeFile: function (filename, content) {
-		    fs.write(filename, content, 'a');
-		},
+  /*
+  Public functions
+  */
+  return {
+    /*
+    Helper function
+    */
+    writeFile: function (filename, content) {
+      fs.write(filename, content, 'a');
+    },
 
-		/*
-		Gather elements or a specific attribute
-		*/
-		getElements: function (query, attribute) {
-		    var elements = document.querySelectorAll(query),
-		        res = [];
+    /*
+    Gather elements or a specific attribute
+    */
+    getElements: function (query, attribute) {
+      var elements = document.querySelectorAll(query),
+        res = [];
 
-		    if(attribute) {
-		        for (var i = 0; i < elements.length; i++) {
-		            res[i] = elements[i].getAttribute(attribute);
-		        }
-		    }else {
-		        res = elements;
-		    }
+      if(attribute) {
+        for (var i = 0; i < elements.length; i++) {
+          res[i] = elements[i].getAttribute(attribute);
+        }
+      }else {
+        res = elements;
+      }
 
-		    return res
-		},
+      return res
+    },
 
-		/*
-		Start the csv on the right foot
-		*/
-		createCsvLine: function (filename, csvArray) {
-		    this.writeFile(filename, csvArray.reduce(function (a, b, index) {
-		        if(index === 0) {
-		            return a + b;
-		        }
-		        return a + ', ' + b;
-		    }, ''));
-		    this.writeFile(filename, '\n');
-		},
+    /*
+    Start the csv on the right foot
+    */
+    createCsvLine: function (filename, csvArray) {
+      this.writeFile(filename, csvArray.reduce(function (a, b, index) {
+        if(index === 0) {
+          return a + b;
+        }
+        return a + ', ' + b;
+      }, ''));
+      this.writeFile(filename, '\n');
+    },
 
-		/*
-		Grab all the things on the page
-		*/
-		evaluateProduct: function (csvItem) {
-	        var el = document.querySelector(csvItem.query),
-	            str = el ? el.innerHTML : '';
+    /*
+    Grab all the things on the page
+    */
+    evaluateProduct: function (csvItem) {
+      var el = document.querySelector(csvItem.query),
+        str = el ? el.innerHTML : '';
 
-	        if(csvItem.regex) {
-	            rx = new RegExp(csvItem.regex, 'g');
-	            str = str.replace(rx, '');
-	        }
+      if(csvItem.regex) {
+        rx = new RegExp(csvItem.regex, 'g');
+        str = str.replace(rx, '');
+      }
 
-	        if(csvItem.attr) {
-	            str = el ? el.getAttribute(csvItem.attr) : '';
-	        }
+      if(csvItem.attr) {
+        str = el ? el.getAttribute(csvItem.attr) : '';
+      }
 
-	        return str;
-		}
-	}
+      return str;
+    }
+  }
 
 })();
 
--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 var Omar = require('./Omar'),
-    strategy = require('./strategies/wdmusic'),
-    casper = require('casper').create({
-        verbose: false,
-        logLevel: 'debug'
-    });
+  strategy = require('./strategies/wdmusic'),
+  casper = require('casper').create({
+      verbose: false,
+      logLevel: 'debug'
+  });
 
 var output = 'dist/output.csv';
 
 var links = [];
 
 casper.start(strategy.url, function() {
-    this.echo(this.getTitle());
-    Omar.createCsvLine(output, strategy.csv.map(function(csv) {
-        return csv.header;
-    }));
-    Omar.createCsvLine(output, []);
+  this.echo(this.getTitle());
+  Omar.createCsvLine(output, strategy.csv.map(function(csv) {
+    return csv.header;
+  }));
+  Omar.createCsvLine(output, []);
 });
 
 casper.then(function () {
-    links = this.evaluate(Omar.getElements, strategy.links, 'href');
-    links.forEach(function (link) {
-        casper.then(function () {
-            this.thenOpen(link, function () {
-                this.echo(this.getTitle());
-                var csvArray = [];
-                strategy.csv.forEach(function (csvItem) {
-                    casper.then(function () {
-                        csvArray.push(this.evaluate(Omar.evaluateProduct, csvItem));
-                    });
-                });
-                casper.then(function () {
-                    Omar.createCsvLine(output, csvArray);
-                });
-            });
+  links = this.evaluate(Omar.getElements, strategy.links, 'href');
+  links.forEach(function (link) {
+    casper.then(function () {
+      this.thenOpen(link, function () {
+        this.echo(this.getTitle());
+        var csvArray = [];
+        strategy.csv.forEach(function (csvItem) {
+          casper.then(function () {
+            csvArray.push(this.evaluate(Omar.evaluateProduct, csvItem));
+          });
         });
-    }.bind(this));
+        casper.then(function () {
+          Omar.createCsvLine(output, csvArray);
+        });
+      });
+    });
+  }.bind(this));
 });
 
 casper.run(function() {
-    this.log('all done', 'info');
-    this.exit();
+  this.log('all done', 'info');
+  this.exit();
 });
 
 
